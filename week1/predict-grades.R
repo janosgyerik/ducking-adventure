f4b408049b77537fdbdf2b0456a40d08ba583712
Janos Gyerik <info@janosgyerik.com>
2014-03-08 18:20:40 +0100

    uncomment some commands that are needed in every run anyway

--- week1/predict-grades.R
+++ week1/predict-grades.R
@@ -1,6 +1,6 @@
 df <- data.frame(
-  x=c(3, 1, 0, 4),
-  y=c(2, 2, 1, 3)
+  x=c(5, 3, 0, 4),
+  y=c(4, 4, 1, 3)
   )
 
 ht <- function(t0, t1, x) {
@@ -14,6 +14,6 @@ jt <- function(t0, t1) {
   sum(mapply(f, df$x, df$y)) / nrow(df) / 2
 }
 
-# jt(0, 1)
-# ht(-1, 0.5, df$x[4])
+jt(0, 1)
+ht(-1, 2, 6)
 # for (x in seq(.3, .5, .01)) print(jt(1, x))
